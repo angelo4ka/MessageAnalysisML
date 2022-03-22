5ec1eabb0957aabcabe8d7f77f480a4857de3a1b
angelo4ka <karina3125@yandex.ru>
2022-03-22 22:07:17 +0300

    Add project files.

---
+++ MessageAnalysisML/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.ML;
+using System;
+using static MessageAnalysisML.MessageAnalysisMLModel;
+
+namespace MessageAnalysisML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите 0, чтобы выйти");
+
+            var inputStr = "";
+
+            while (inputStr != "0")
+            {
+                inputStr = Console.ReadLine();
+                var input = new ModelInput();
+                input.Message = inputStr;
+
+                var predictionResult = MessageAnalysisMLModel.Predict(input);
+
+                Console.WriteLine(predictionResult.PredictedLabel == 1 ? "Это спам" : "Обычное сообщение");
+                Console.WriteLine("---");
+            }
+        }
+    }
+}
